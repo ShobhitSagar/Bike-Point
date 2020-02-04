5322f65c0f5064eaaf9b889559ae132bd53c1316
Shobhit Sagar <shobhitsagar109@gmail.com>
2020-02-04 20:05:23 +0530

    Added detail activity

--- app/src/main/java/com/example/bikepoint/MainActivity.java
+++ app/src/main/java/com/example/bikepoint/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.bikepoint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout bike;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bike = findViewById(R.id.bike);
+
+        bike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
